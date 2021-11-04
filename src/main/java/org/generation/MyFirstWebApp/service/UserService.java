96163d947912681a1fcb785eeb99a0077a59beb1
Ricardo Daniel <Ricperez0101@gmail.com>
2021-11-04 10:11:34 -0600

    UserService

---
+++ src/main/java/org/generation/MyFirstWebApp/service/UserService.java
@@ -0,0 +1,20 @@
+package org.generation.MyFirstWebApp.service;
+
+import org.generation.MyFirstWebApp.model.User;
+
+import java.util.List;
+
+public interface UserService
+{
+    List<User> getAll();
+
+    void add(User user);
+
+    void remove(User user);
+
+    int size();
+
+    User getUser(int position);
+
+
+}
